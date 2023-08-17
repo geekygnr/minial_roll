0d1f799caa49aadcdf7b2d43ed9d16dcbc6a111a
Bryan Heisler <bheisler@alumni.uoguelph.ca>
2023-08-17 16:20:45 -0400

    Add framework for squad.

---
+++ src/Entity/Squad.php
@@ -0,0 +1,202 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\minial_roll\Entity;
+
+use Drupal\Core\Entity\EntityChangedTrait;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\RevisionableContentEntityBase;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\minial_roll\SquadInterface;
+use Drupal\user\EntityOwnerTrait;
+
+/**
+ * Defines the squad entity class.
+ *
+ * @ContentEntityType(
+ *   id = "minial_roll_squad",
+ *   label = @Translation("Squad"),
+ *   label_collection = @Translation("Squads"),
+ *   label_singular = @Translation("squad"),
+ *   label_plural = @Translation("squads"),
+ *   label_count = @PluralTranslation(
+ *     singular = "@count squads",
+ *     plural = "@count squads",
+ *   ),
+ *   bundle_label = @Translation("Squad type"),
+ *   handlers = {
+ *     "list_builder" = "Drupal\minial_roll\SquadListBuilder",
+ *     "views_data" = "Drupal\views\EntityViewsData",
+ *     "access" = "Drupal\minial_roll\SquadAccessControlHandler",
+ *     "form" = {
+ *       "add" = "Drupal\minial_roll\Form\SquadForm",
+ *       "edit" = "Drupal\minial_roll\Form\SquadForm",
+ *       "delete" = "Drupal\Core\Entity\ContentEntityDeleteForm",
+ *       "delete-multiple-confirm" = "Drupal\Core\Entity\Form\DeleteMultipleForm",
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *     },
+ *   },
+ *   base_table = "minial_roll_squad",
+ *   data_table = "minial_roll_squad_field_data",
+ *   revision_table = "minial_roll_squad_revision",
+ *   revision_data_table = "minial_roll_squad_field_revision",
+ *   show_revision_ui = TRUE,
+ *   translatable = TRUE,
+ *   admin_permission = "administer minial_roll_squad types",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "revision" = "revision_id",
+ *     "langcode" = "langcode",
+ *     "bundle" = "bundle",
+ *     "label" = "label",
+ *     "uuid" = "uuid",
+ *     "owner" = "uid",
+ *   },
+ *   revision_metadata_keys = {
+ *     "revision_user" = "revision_uid",
+ *     "revision_created" = "revision_timestamp",
+ *     "revision_log_message" = "revision_log",
+ *   },
+ *   links = {
+ *     "collection" = "/admin/content/squad",
+ *     "add-form" = "/squad/add/{minial_roll_squad_type}",
+ *     "add-page" = "/squad/add",
+ *     "canonical" = "/squad/{minial_roll_squad}",
+ *     "edit-form" = "/squad/{minial_roll_squad}/edit",
+ *     "delete-form" = "/squad/{minial_roll_squad}/delete",
+ *     "delete-multiple-form" = "/admin/content/squad/delete-multiple",
+ *   },
+ *   bundle_entity_type = "minial_roll_squad_type",
+ *   field_ui_base_route = "entity.minial_roll_squad_type.edit_form",
+ * )
+ */
+final class Squad extends RevisionableContentEntityBase implements SquadInterface {
+
+  use EntityChangedTrait;
+  use EntityOwnerTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preSave(EntityStorageInterface $storage): void {
+    parent::preSave($storage);
+    if (!$this->getOwnerId()) {
+      // If no owner has been set explicitly, make the anonymous user the owner.
+      $this->setOwnerId(0);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type): array {
+
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['label'] = BaseFieldDefinition::create('string')
+      ->setRevisionable(TRUE)
+      ->setTranslatable(TRUE)
+      ->setLabel(t('Label'))
+      ->setRequired(TRUE)
+      ->setSetting('max_length', 255)
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'hidden',
+        'type' => 'string',
+        'weight' => -5,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['status'] = BaseFieldDefinition::create('boolean')
+      ->setRevisionable(TRUE)
+      ->setLabel(t('Status'))
+      ->setDefaultValue(TRUE)
+      ->setSetting('on_label', 'Enabled')
+      ->setDisplayOptions('form', [
+        'type' => 'boolean_checkbox',
+        'settings' => [
+          'display_label' => FALSE,
+        ],
+        'weight' => 0,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'type' => 'boolean',
+        'label' => 'above',
+        'weight' => 0,
+        'settings' => [
+          'format' => 'enabled-disabled',
+        ],
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['description'] = BaseFieldDefinition::create('text_long')
+      ->setRevisionable(TRUE)
+      ->setTranslatable(TRUE)
+      ->setLabel(t('Description'))
+      ->setDisplayOptions('form', [
+        'type' => 'text_textarea',
+        'weight' => 10,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'type' => 'text_default',
+        'label' => 'above',
+        'weight' => 10,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['uid'] = BaseFieldDefinition::create('entity_reference')
+      ->setRevisionable(TRUE)
+      ->setTranslatable(TRUE)
+      ->setLabel(t('Author'))
+      ->setSetting('target_type', 'user')
+      ->setDefaultValueCallback(self::class . '::getDefaultEntityOwner')
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'settings' => [
+          'match_operator' => 'CONTAINS',
+          'size' => 60,
+          'placeholder' => '',
+        ],
+        'weight' => 15,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'author',
+        'weight' => 15,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Authored on'))
+      ->setTranslatable(TRUE)
+      ->setDescription(t('The time that the squad was created.'))
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'timestamp',
+        'weight' => 20,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'datetime_timestamp',
+        'weight' => 20,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['changed'] = BaseFieldDefinition::create('changed')
+      ->setLabel(t('Changed'))
+      ->setTranslatable(TRUE)
+      ->setDescription(t('The time that the squad was last edited.'));
+
+    return $fields;
+  }
+
+}
---
+++ src/Entity/SquadType.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types = 1);
+
+namespace Drupal\minial_roll\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBundleBase;
+
+/**
+ * Defines the Squad type configuration entity.
+ *
+ * @ConfigEntityType(
+ *   id = "minial_roll_squad_type",
+ *   label = @Translation("Squad type"),
+ *   label_collection = @Translation("Squad types"),
+ *   label_singular = @Translation("squad type"),
+ *   label_plural = @Translation("squads types"),
+ *   label_count = @PluralTranslation(
+ *     singular = "@count squads type",
+ *     plural = "@count squads types",
+ *   ),
+ *   handlers = {
+ *     "form" = {
+ *       "add" = "Drupal\minial_roll\Form\SquadTypeForm",
+ *       "edit" = "Drupal\minial_roll\Form\SquadTypeForm",
+ *       "delete" = "Drupal\Core\Entity\EntityDeleteForm",
+ *     },
+ *     "list_builder" = "Drupal\minial_roll\SquadTypeListBuilder",
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *     },
+ *   },
+ *   admin_permission = "administer minial_roll_squad types",
+ *   bundle_of = "minial_roll_squad",
+ *   config_prefix = "minial_roll_squad_type",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *     "uuid" = "uuid",
+ *   },
+ *   links = {
+ *     "add-form" = "/admin/structure/minial_roll_squad_types/add",
+ *     "edit-form" = "/admin/structure/minial_roll_squad_types/manage/{minial_roll_squad_type}",
+ *     "delete-form" = "/admin/structure/minial_roll_squad_types/manage/{minial_roll_squad_type}/delete",
+ *     "collection" = "/admin/structure/minial_roll_squad_types",
+ *   },
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *     "uuid",
+ *   },
+ * )
+ */
+final class SquadType extends ConfigEntityBundleBase {
+
+  /**
+   * The machine name of this squad type.
+   */
+  protected string $id;
+
+  /**
+   * The human-readable name of the squad type.
+   */
+  protected string $label;
+
+}
